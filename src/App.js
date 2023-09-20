ac8380ad28cc47ffed467b3c0194480a0c0a0c88
Matheus Bombassei <matheusbombassei@outlook.com>
2023-09-20 13:06:17 -0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,25 +7,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 const App = () => {
-  const [nameFilter, setNameFilter] = useState('');
-  const [persons, setPersons] = useState([]);
+  const [setPersons] = useState([]);
 
   useEffect(() => {
-    // Use o método fetch para fazer a solicitação HTTP
     fetch('https://vigilant-api-a2xyukeyka-uc.a.run.app/fbi/getallwanted?page=1')
       .then((response) => response.json())
       .then((data) => setPersons(data))
       .catch((error) => console.error('Erro ao buscar dados:', error));
   }, []);
-
-  // Função para dividir o array de persons em grupos de 3
-  const chunkArray = (array, chunkSize) => {
-    const result = [];
-    for (let i = 0; i < array.length; i += chunkSize) {
-      result.push(array.slice(i, i + chunkSize));
-    }
-    return result;
-  };
 
   return (
     <Router>
@@ -34,13 +23,10 @@ const App = () => {
         <div className="row mt-3">
           <div className="col-md-10" id="wanted-persons">
             <Routes>
-              {/* Rota para a página do FBI */}
               <Route path="/fbi" element={<FbiPage/>}>
               </Route>
-              {/* Rota para a página da Interpol */}
               <Route path="/interpol" element={<InterpolPage/>}>
               </Route>
-              {/* Rota padrão (outras páginas) */}
               <Route path="/">
               </Route>
             </Routes>
