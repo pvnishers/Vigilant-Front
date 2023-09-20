ee5f2ddbcee1c6066e1f9dd03537f21f40be8fad
Matheus Bombassei <matheusbombassei@outlook.com>
2023-09-20 12:46:50 -0300

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-            <a class="navbar-brand" href="#">Vigilant Core</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/fbi">FBI</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/interpol">Interpol</a>
-                    </li>
-                </ul>
-            </div>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Vigilant Core</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/fbi">FBI</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/interpol">Interpol</Link>
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
   );
 };
